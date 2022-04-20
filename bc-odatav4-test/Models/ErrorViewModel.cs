e8d6bdf391769aaf7ec3747d9630ca530b021210
Carlos Aragon Pelayo <carlete.aragon@gmail.com>
2022-04-20 12:16:46 +0200

    Project creation

---
+++ bc-odatav4-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bc_odatav4_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
