d9b88dbe33db622f86255008f66923db0a1717f1
Carlos Aragon Pelayo <carlete.aragon@gmail.com>
2022-04-20 17:51:57 +0200

    Always 401 unauthorized
    TODO change access method.

--- bc-odatav4-test/Controllers/HomeController.cs
+++ bc-odatav4-test/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using BC;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,7 +21,13 @@ namespace bc_odatav4_test.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //Base URL for Business Central OData services
+            var baseUrl = "http://vsspc054:7048/BC190/ODataV4/";
+            //Data context
+            var context = new BC.NAV(new Uri(baseUrl));
+
+            IEnumerable<BC.Employees> showList = context.Employees.Execute();
+            return View(showList);
         }
 
         public IActionResult Privacy()
